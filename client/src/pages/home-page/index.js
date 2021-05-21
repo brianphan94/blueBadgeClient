123318477c480c2e6a16539234e94cc2d55e153e
brianphan94 <brianphan317@gmail.com>
2021-05-20 21:43:46 -0400

    home feed

--- client/src/pages/home-page/index.js
+++ client/src/pages/home-page/index.js
@@ -1,18 +1,61 @@
+import React from 'react';
 import './home.scss'
 import {Button} from 'reactstrap'
+import {useState, useEffect} from 'react';
+import {Card} from 'reactstrap';
+
+import UserLogo from '../../userlogo.svg';
 
 const Home = (props) => {
 
-    //document.body.style = 'background: white'
+    const [reviews, getReviews] = useState([])
+
+    let everyPost = () => {
+        fetch('http://localhost:4040/review/all', {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            }),
+        }) .then(res => res.json())
+        .then(data => {
+            getReviews(data.review)
+            console.log(data)
+        })
+    }
+
+    useEffect(() => {
+        everyPost()
+    }, [])
+    
 
     return(
         <div>
-            <h1>
-            User home page
-            </h1>
+            <h1>User home page</h1>
+            <div className='review-feed-box'>
+            <Card>
+                {reviews.length > 0 ? (
+                    reviews.map((review) => (
+                        <li className='review'>
+                            <h2>{review.reviewTitle}</h2>
+                            <p className='review-body'>{review.reviewBody}</p>
+                            <div className='review-footer'>
+                                <img className='userlogo' src={UserLogo} />
+                                <p>Review by: {review.email}</p>
+                            </div>
+                            
+                        </li>
+                    ))
+                ) : (
+                    <h1>Loading...</h1>
+                )}
+            </Card>
+        </div>
             <Button onClick={props.clickLogout}>Logout</Button>
         </div>
     )
 }
+
+
 
 export default Home
