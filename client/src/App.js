0f79b073378df761a9c64adcf21af53ae6015b9a
Jason-Henson <jason.a.henson@gmail.com>
2021-05-21 14:55:58 -0400

    push before merge

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+//import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Post Review</h1>
+
+    </div>
+  );
+}
+
+export default App;
