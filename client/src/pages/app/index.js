194cc433e1897235ef5f69ee218e72bd8c5a3908
aymiller200 <aymiller200@gmail.com>
2021-06-06 15:27:48 -0400

    just updated some styling

--- client/src/pages/app/index.js
+++ client/src/pages/app/index.js
@@ -2,13 +2,10 @@ import './App.scss';
 
 import { useState, useEffect } from 'react'
 
-
 import Auth from '../landing/auth/'
 import Header from '../../components/header'
 import Sidebar from '../../components/home-page-sidebar'
 import Footer from '../../components/footer/footer'
-//import Home from '../home-page/index'
-
 
 function App() {
   const [token, setSessionToken] = useState('')
@@ -37,21 +34,15 @@ function App() {
 
   const protectedViews = () => {
     return (
-      token === localStorage.getItem('token') ? <Sidebar token={token} userTitle={userTitle} clickLogout={clearToken} /> : <Auth setUserTitle={setUserTitle} updateToken={updateToken} />
-      
-      
+      token === localStorage.getItem('token') ? <Sidebar token={token} userTitle={userTitle} clickLogout={clearToken} /> : <Auth setUserTitle={setUserTitle} updateToken={updateToken} />          
       )
   }
-
-  
 
   return (
     <div className="App">
       <Header />
       {protectedViews()}
       <Footer />
-
-
     </div>
   );
 }
